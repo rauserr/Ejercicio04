7a08dfd803a0a6f948ec8cc8cd897f120d94daf0
crafer32 <raul.serrano.martinez@cieep.com>
2023-11-13 23:53:15 +0100

    Ejercicio04

---
+++ app/src/main/java/raul/serrano/ejercicio04/DesencriptarActivity.java
@@ -0,0 +1,25 @@
+package raul.serrano.ejercicio04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class DesencriptarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desencriptar);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if(bundle.isEmpty()){
+            String password = bundle.getString("PASS");
+            Toast.makeText(this, "Contraseña es "+password, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
--- app/src/main/java/raul/serrano/ejercicio04/MainActivity.java
+++ app/src/main/java/raul/serrano/ejercicio04/MainActivity.java
@@ -1,13 +1,47 @@
 package raul.serrano.ejercicio04;
 
-import  androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import javax.crypto.spec.DESedeKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtPassword;
+    private Button btnDesencriptar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniciailzarVista();
+
+        btnDesencriptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String password =txtPassword.getText().toString();
+
+                if(password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "FALTAN DATOS", Toast.LENGTH_SHORT).show();
+                }else{
+                    //ENVIAR INFORMACIÓN A LA SEGUNDA ACTIVIDAD
+                    Intent intent = new Intent(MainActivity.this, DesencriptarActivity.class);
+                    //PASAR INFORMACIÓN
+                    Bundle bundle = new Bundle();
+                    bundle.putString("PASS", password);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    private void iniciailzarVista() {
+        txtPassword = findViewById(R.id.txtPasswordMain);
+        btnDesencriptar = findViewById(R.id.btnDesencriptarMain);
     }
 }
